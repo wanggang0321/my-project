7dd565f16eefb548b8bd330c30c00d405753cdbe
wanggang0321 <578620382@qq.com>
2018-02-23 14:49:38 +0800

    添加Vue图标，修改样式居中

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div class="hello">
+    <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <firstcomponent></firstcomponent>
     <router-link to="/view1">Go to view1</router-link>
@@ -24,3 +25,15 @@ export default {
   components: {firstcomponent}
 }
 </script>
+
+<!-- 样式代码 -->
+<style>
+.hello {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
